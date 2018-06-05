93ccc402ea178c79566f43372378f522085bd6a9
EmiKnape <enknape@aol.com>
2018-06-05 21:47:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(number)
   if number % 3 ==0 && number % 5 == 0 
     "Fizzbuzz"
-  if number % 3 == 0 
-    "Fizz"
-    elsif number % 5 == 0
+  if number % 5 == 0 
     "Buzz"
-  
+    elsif number % 3 == 0
+    "Fizz"
   end 
+end 
   
 end 
 puts fizzbuzz(15)
